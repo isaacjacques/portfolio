ed9b3813171f150535192dde5654a0ea6d4b5ff2
isaacjacques <you@example.comname>
2023-06-23 22:42:30 -0400

    MVP completed and ready to host

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,21 @@
-import { GiAbstract082 } from 'react-icons/gi'
+import { GiAmplitude } from 'react-icons/gi'
 
 const Header = () => {
   return (
-    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-3/8 lg:flex-col lg:justify-between lg:py-24">
       <div>
         <a className="flex flex-row text-paledogwood-300 hover:text-rosequartz-400" href="/" target="_blank" rel="noreferrer">
-          <GiAbstract082 className="pt-2" size="45"/>
-          <h1 className="font-bold text-5xl text-isabelline-100 hover:text-rosequartz-400">
+          <GiAmplitude size="46" className="pt-1 pr-1"/>
+          <h1 className="font-bold whitespace-nowrap text-5xl text-isabelline-100 hover:text-rosequartz-400">
             Isaac Jacques
           </h1>
         </a>
         <h2 className="font-medium text-2xl text-paledogwood-300">Sofware Developer</h2>
+        <div className="text-lg text-isabelline-100"> 
+          <p className="mb-8">
+            "I build things."
+          </p>
+        </div>
       </div>
     </header>
   );
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -9,7 +9,7 @@ const Layout = () => {
             <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 font-sans">
                 <div className="lg:flex justify-between">
                     <Header />
-                    <main className="pt-24 lg:w-1/2 lg:py-24">
+                    <main className="pt-24 pl-6 lg:w-5/8 lg:py-24">
                         <Outlet />
                     </main>
                 </div>
